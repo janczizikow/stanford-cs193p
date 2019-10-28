6ab4365d3c4f23cfdd1666868e34a8da9e086d3a
Jan <jan.czizikow@gmail.com>
2019-10-28 23:10:52 +0100

    shuffle()

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -39,7 +39,6 @@ class Concentration {
             let card = Card()
             cards += [card, card]
         }
-        // TODO: Shuffle the cards
-        // cards.shuffle() // Check if it works as expected
+        cards.shuffle()
     }
 }
